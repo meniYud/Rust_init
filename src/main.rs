a6bda101c87e35e0a46bb2a986fd4ef990709da1
meniYud <menyud@gmail.com>
2024-08-16 16:48:19 +0300

    flow control: conditions, and enums match

--- src/main.rs
+++ src/main.rs
@@ -284,12 +284,91 @@ Would you like me to clarify any part of this explanation further?
 */
 }
 
+fn max_expression(arg1: i32, arg2: i32) -> i32{
+    if arg1 > arg2 {
+        arg1
+    } else {
+        arg2
+    }
+}
+fn max_statement(arg1: i32, arg2: i32) -> i32{
+    // if is an expression so we can write
+    let max = if arg1 > arg2 {
+        arg1
+    } else {
+        arg2
+    };
+    max
+}
 
+fn nested_condition(arg1: i32, arg2: i32) -> bool{
+    if arg1 % 2 == 0 {
+        println!("arg1 is even {}", arg1);
+        true
+    } else {
+        println!("arg1 is odd {}", arg1);
+        if arg2 % 2 == 0 {
+            println!("arg2 is even {}", arg2);
+            return true;
+        } else {
+            println!("arg2 is odd {}", arg2);
+            return false;
+        }
+    }
+}
 
+fn and_or() -> (bool, bool) {
+    let a = false;
+    let b = true;
+
+    return (a && b, a || b);
+}
+
+fn match_enums(){
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter,
+    }
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
+
+    println!("Penny in cents: {}", value_in_cents(Coin::Penny));
+    println!("Nickel in cents: {}", value_in_cents(Coin::Nickel));
+    println!("Dime in cents: {}", value_in_cents(Coin::Dime));
+    println!("Quarter in cents: {}", value_in_cents(Coin::Quarter));
+}
+
+fn flow_control(){
+    let max1 = max_expression(5, 4);
+    println!("max_expression value is {}", max1);
+    
+    let max2 = max_statement(5, 4);
+    println!("max_statement value is {}", max2);
+    
+    let oddity = nested_condition(5, 4);
+    println!("oddity is {}", oddity);
+
+    let (a_and_b, a_or_b) = and_or();
+    println!("a or b is true: {}", a_or_b);
+    println!("a and b is true: {}", a_and_b);
+
+    match_enums();
+}
 
 fn main(){
     // vars();
     // data_types();
     // another_function(42, 'a', "hello");
-    statements_vs_expressions()
+    // statements_vs_expressions()
+
+    flow_control()
 }

4aa015ddb7b31f0691fb3d60303378f91bf848f0
meniYud <menyud@gmail.com>
2024-09-06 17:00:56 +0300

    paths: use nested resources - no implementation

---
+++ src/paths/nested_pathes.rs
@@ -0,0 +1,12 @@
+// use std::cmp::Oredering;
+use std::{cmp::Oredering, io};
+
+// use std::io;
+// use std::io::Write;
+use std::io::{self, Write};
+
+use std::collections::*;
+
+pub mod nested{
+    //nothing to show here
+}

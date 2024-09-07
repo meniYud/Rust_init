b546d92c0f1faea8e504a6600a00ba59e240c827
meniYud <menyud@gmail.com>
2024-09-07 12:03:33 +0300

    vectors: enum as type structure of multitype vector

--- src/advanced_data_types/collections.rs
+++ src/advanced_data_types/collections.rs
@@ -66,4 +66,20 @@ pub fn vectors(){
     }
     println!("Updated vector: {:?}", vec6);
 
+    //multi-type vector
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String)
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Float(3.1415),
+        SpreadsheetCell::Text(String::from("Some text")),
+        SpreadsheetCell::Float(2.7)
+    ];
+    println!("Multy-type vector: {:?}", row);
+
 }

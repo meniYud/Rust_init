911e43912a9c677d95ba13ad4e1788e0dcc249a3
meniYud <menyud@gmail.com>
2024-09-12 09:32:40 +0300

    error handling: recoverables - Result with match

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -2,8 +2,12 @@
 mod unrecoverables;
 use unrecoverables::panicing;
 
+mod recoverables;
+use recoverables::result_enum;
+
 
 
 pub fn error_handling_main(){
-    panicing()
+    // panicing()
+    result_enum()
 }
---
+++ src/error_handling/recoverables.rs
@@ -0,0 +1,32 @@
+/*
+Source: https://www.youtube.com/watch?v=NBFH3BnCIvU&list=PLPoSdR46FgI412aItyJhj2bF66cudB6Qs&index=23
+
+
+By the end of this video, you will learn how to handle recoverable errors in Rust using the Result type.
+We start with a basic example of recoverable errors and demonstrate how to use the match statement to handle different error cases. 
+
+You'll see how to manage results with if-else statements and get familiar with shortcuts like unwrap and expect. 
+
+We also cover error propagation and introduce the ? operator for more streamlined error handling.
+Discover where the ? operator can be used, including its application in the main function. 
+
+*/
+
+use std::fs::File;
+use std::io::ErrorKind;
+
+pub fn result_enum(){
+    let greeting_from_file = File::open("hello.txt");
+    let greeting_file = match greeting_from_file {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(error) => panic!("Unable to create the file: {:?}", error),
+            },
+            other_error => {
+                panic!("File found, but other error occured: {:?}", other_error);
+            }
+        }
+    };
+}
